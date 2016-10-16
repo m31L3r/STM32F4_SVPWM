ed8b82bde1e9a0990183e5a70a0639fd569586c6
Michael Meiler <michael-meiler@gmx.de>
2016-10-16 14:42:36 +0200

    Add files via upload

--- irq_handle.c
+++ irq_handle.c
@@ -11,82 +11,67 @@
 *                   Includes
 *
 *********************************************************************/
-#include "stm32f4xx.h"
-#include "stm32f4xx_conf.h"
+#include "irq_handle.h"
 
-/*********************************************************************
-*
-*                   Defines
-*
-*********************************************************************/
-#define _MAX_BUFFER_LENGTH 10
-
-/*********************************************************************
-*
-*                   Variables
-*
-*********************************************************************/
-volatile char string_buffer [_MAX_BUFFER_LENGTH + 1];
-
-
-
-
+volatile uint16_t blabla;
 /*********************************************************************
  *
- *					Tim2 InterruptHandle
+ *					Tim2 IRQ
  *
  ********************************************************************/
-/*
+
 void TIM2_IRQHandler(void) {
 
 	/* Make sure that interrupt flag is set */
-/*	if (TIM_GetITStatus(TIM2, TIM_IT_Update) != RESET) {
+	if (TIM_GetITStatus(TIM2, TIM_IT_Update) != RESET) {
 
 	  GPIO_SetBits(GPIOD, GPIO_Pin_0);	// Debugging, watch Calculation Time
 
 	  /* Measure Ia and Ib and Theta */
-/*	  Theta = TIM_GetCounter(TIM8) * 0.087890625; // 0.087890625 = 360°/4096
-	  Ia = (ADC_GetConversionValue(ADC1)/4096)*_U_currentmeasue; // TO DO: multiply with current/voltage ratio
-	  Ib = (ADC_GetConversionValue(ADC1)/4096)*_U_currentmeasue; // TO DO: multiply with current/voltage ratio
+	  //Theta = ((uint16_t) TIM_GetCounter(TIM8)) * 0.001533981; // 0.001533981 = 2pi/4096
+	  //Ia = (ADC_GetConversionValue(ADC1)/4095)*_MAX_CURRENT;
+	  Ib = (ADC_GetConversionValue(ADC2)/4095)*_MAX_CURRENT;
+	  blabla = ADC_GetConversionValue(ADC3);
 
 	  /* Output angle counter value*/
-	  //USART_SendData(USART1, 'ok');
+	  //USART_SendData(USART2, 'ok');
 
-	  /* Transform Ia and Ib in Iq*/
-/*	  Iq_real = transform(Ia, Ib, Theta);
+	  /* Calculates sine and cosine for park transformation */
+	  arm_sin_cos_f32(Theta, &sinevalue, &cosinevalue);
+
+	  /* Transform Ia and Ib in Iq */
+	  Iq_real = transform(Ia, Ib, Theta);
 
 	  /* Control Iq and return Uq */
-/*	  Uq = PI_control(Iq_ref, Iq_real);
+	  Uq = PI_control(Iq_ref, Iq_real);
 
 	  /* Transform Uq in U_alpha */
-	  // arm_inv_park_f32();
+	  arm_inv_park_f32(Ud, Uq, &U_alpha, &U_beta, sinevalue, cosinevalue);
 
 	  /* Calculate a new pulse width */
-/*	  svpwm();
+	  svpwm();
 
 	  TIM1->CCR1 = *switchtime[0];
 	  TIM1->CCR2 = *switchtime[1];
 	  TIM1->CCR3 = *switchtime[2];
 
-	  //Theta += 0.019;
-	  //if (Theta >= (2*M_PI))
-	  //	Theta = 0;
+	  Theta += 0.019;
+	  if (Theta >= (2*M_PI))
+	  	Theta = 0;
 
-	  // ADC 0V min 3.3V max
-	  // U_alpha = (ADC_GetConversionValue(ADC1)/4096)*_U_DC;
 
 	  /* Clear Interrupt Flag */
-/*	  TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
+	  TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
 
 	  GPIO_ResetBits(GPIOD, GPIO_Pin_0);	// Debugging
   }
 }
-*/
+
 
 
 /*********************************************************************
  *
- *					InterruptHandle for External Signal PC8 (Reset Encoder Value)
+ *					IRQ for External Signal PC8 (Reset Encoder Value)
  *
  ********************************************************************/
 
@@ -97,9 +82,10 @@ void EXTI9_5_IRQHandler(void) {
 
     	/* Reset Counter */
     	TIM_SetCounter(TIM8, 0x00);
+    	USART_SendData(USART2, 0x01);
 
         /* Clear interrupt flag */
-        EXTI_ClearITPendingBit(EXTI_Line12);
+        EXTI_ClearITPendingBit(EXTI_Line8);
     }
 }
 
@@ -115,7 +101,7 @@ void USART2_IRQHandler(void) {
 
 	if (USART_GetITStatus(USART2, USART_IT_RXNE)) {
 		static int count = 0;
-		char ch = USART2->DR;	// Read clears ITPendingBit
+		uint8_t ch = USART2->DR;	// Read clears ITPendingBit
 
 /*------Here you can modify what to do when data is received------*/
 		if ((ch != '\n') && (count < _MAX_BUFFER_LENGTH)) {
